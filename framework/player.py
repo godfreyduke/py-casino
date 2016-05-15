02183d2e58b123499cc8638421693fc7e13b85bf
Allen <allen@parentsware.com>
2016-05-15 00:45:59 -0700

    added player class, added __init__

---
+++ framework/player.py
@@ -0,0 +1,17 @@
+from cards import Hand
+
+class Player(object):
+    def __init__(self, name, credits=100, hand=Hand()):
+        self.name = name
+        self.credits = credits
+        self.hand = hand
+        
+    def bet(self, amount):
+        if amount > self.credits:
+            raise ValueError('Not enough credits')
+        else:
+            self.credits = self.credits - amount        
+
+class Dealer(Player):
+    def __init__(self):
+        super(Dealer, self).__init__(name='The Dealer', credits=1000000000)
