5b4ca87ade89240042025706c342676b8482efc3
Mike Herold <archangel.herold@gmail.com>
2016-05-19 22:52:12 -0700

    shifted some show commands around so we have more control over when the UI is updated. added main entry point for video poker.

--- games/videopoker.py
+++ games/videopoker.py
@@ -1,4 +1,6 @@
 from framework import Deck, Hand
+from framework.terminal import Terminal
+from types import IntType
 
 
 class VideoPoker:
@@ -16,10 +18,38 @@ class VideoPoker:
         return self.show()
 
     def discard(self, indicies):
+        if type(indicies) is IntType:
+            indicies = [indicies]
+
         self.hand.discard(index=indicies)
         self.hand.draw(times=len(indicies))
-        return self.show()
 
     def show(self):
+        card_index = 0
+        for card in self.hand:
+            card_index += 1
+            print "%i) %s " % (card_index, card)
+
         return self.hand.cards
 
+
+if __name__ == "__main__":
+    poker = VideoPoker()
+
+    poker.start()
+    discard_1_input = raw_input("Pick cards to discard... (ex. 1 4):\n")
+
+    if len(discard_1_input) > 1:
+        discard_1_input = discard_1_input.split(" ")
+    else:
+        discard_1_input = [discard_1_input]
+
+    discards = []
+
+    for discard in discard_1_input:
+        discards.append(int(discard) - 1)
+
+    poker.discard(discards)
+    Terminal.clear_screen()
+    print "Here are your new cards: \n"
+    poker.show()
